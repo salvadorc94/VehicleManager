88893aa8b913d3b36029f07eb02f3a4ad0e5ef0c
JDAlvarenga <00069216@uca.edu.sv>
2018-07-01 19:41:29 -0600

    Created classes:  AppDAO  AppDatabase  AppRepository  AppViewModel

---
+++ app/src/main/java/com/example/maris/vehiclemanager/Database/AppDAO.java
@@ -0,0 +1,14 @@
+package com.example.maris.vehiclemanager.Database;
+
+import android.arch.persistence.room.Dao;
+
+/**
+ * Global database DAO
+ */
+
+@Dao
+public interface AppDAO  {
+    //Category
+
+
+}
