caf87e6e22f44befdd299641d20344556b990b03
salvadorc94 <secg.1994@gmail.com>
2018-07-05 11:49:53 -0600

    Added some resources in string

--- app/src/main/java/com/example/maris/vehiclemanager/Fragments/DateFilterFragment.java
+++ app/src/main/java/com/example/maris/vehiclemanager/Fragments/DateFilterFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import com.example.maris.vehiclemanager.R;
 
@@ -19,14 +20,19 @@ import com.example.maris.vehiclemanager.R;
  * create an instance of this fragment.
  */
 public class DateFilterFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
+
+
+    //private String mParam1;
+    //private String mParam2;
+
+    //Declarar Spinners
+    Spinner date_spinner;
+    Spinner category_spinner;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,31 +40,26 @@ public class DateFilterFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DateFilterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DateFilterFragment newInstance(String param1, String param2) {
         DateFilterFragment fragment = new DateFilterFragment();
+        /*
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+        */
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+        /*if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        }*/
+
+
     }
 
     @Override
@@ -68,7 +69,6 @@ public class DateFilterFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_date_filter, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -103,7 +103,6 @@ public class DateFilterFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
