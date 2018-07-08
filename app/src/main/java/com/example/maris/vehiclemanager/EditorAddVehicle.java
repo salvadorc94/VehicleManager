2719278354a788be699ddf34c9de3672d2053edb
MaryBenitez <marisol.dominguezxp@gmail.com>
2018-07-07 23:10:51 -0600

    Actividad "EditorAddVehicle", ya funciona la opcion de tomar fotografía, además de crear un directorio especifico para el almacenamiento de las fotos

--- app/src/main/java/com/example/maris/vehiclemanager/EditorAddVehicle.java
+++ app/src/main/java/com/example/maris/vehiclemanager/EditorAddVehicle.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 
 import android.os.Environment;
@@ -40,11 +41,19 @@ public class EditorAddVehicle extends AppCompatActivity {
     FloatingActionButton btn_camara;
     private EditText edit_name_vehicle, edit_model_vehicle, edit_year_vehicle, edit_odometer_vehicle, edit_carplate_vehicle, edit_gasoline_vehicle;
     private Uri imageUri;
-    private Button accept;
+    private Button btn_accept_vehicle;
     private Vehicle vehicle;
 
-    public static final int TAKE_PICTURE_VEHICLE = 100;
+    //public static final int TAKE_PICTURE_VEHICLE = 100;
     public static final String EXTRA_VEHICLE = "EXTRA_VEHICLE";
+
+    private final String FILE_IMAGE = "VehicleManager/";
+    private final String ROUTE_IMAGE = FILE_IMAGE+"Photos";
+
+    final int CODE_SELECTED_GALLERY = 10;
+    final int CODE_SELECTED_CAMERA = 20;
+
+    String path;
 
 
     @Override
@@ -60,7 +69,7 @@ public class EditorAddVehicle extends AppCompatActivity {
         edit_odometer_vehicle = findViewById(R.id.id_odom_add_edit_v);
         edit_carplate_vehicle = findViewById(R.id.id_placa_add_edit_v);
         edit_gasoline_vehicle = findViewById(R.id.id_gesoline_add_edit_v);
-        accept = findViewById(R.id.btn_accept_add_edit_v);
+        btn_accept_vehicle = findViewById(R.id.btn_accept_add_edit_v);
 
         Intent intent = getIntent();
         vehicle = intent.hasExtra(EXTRA_VEHICLE) ? (Vehicle) intent.getParcelableExtra(EXTRA_VEHICLE) : (new Vehicle());
@@ -95,7 +104,7 @@ public class EditorAddVehicle extends AppCompatActivity {
                         }).create().show();*/
 
 
-                String[] opciones = getResources().getStringArray(R.array.selection_gallery_or_camara);
+                String[] opciones = getResources().getStringArray(R.array.selection_gallery_or_camera);
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.select_dialog_item, opciones);
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(EditorAddVehicle.this);
                 alertDialogBuilder.setTitle(R.string.select_option);
@@ -118,6 +127,15 @@ public class EditorAddVehicle extends AppCompatActivity {
                 }).create().show();
 
 
+            }
+        });
+
+        btn_accept_vehicle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivityMenu.class);
+                Toast.makeText(getApplicationContext(),R.string.Successful_Event,Toast.LENGTH_SHORT).show();
+                startActivity(intent);
             }
         });
     }
@@ -154,20 +172,41 @@ public class EditorAddVehicle extends AppCompatActivity {
 
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         intent.setType("image/*");
-        startActivityForResult(intent.createChooser(intent,"Select application..."),10);
+        startActivityForResult(intent.createChooser(intent,"Select option..."),CODE_SELECTED_GALLERY);
 
     }
 
     private void OpenCamara() {
 
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        /*String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String nameFile  = timeStamp+".jpg";
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         File photo = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), nameFile);
 
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
         imageUri = Uri.fromFile(photo);
-        startActivityForResult(intent, TAKE_PICTURE_VEHICLE);
+        startActivityForResult(intent, TAKE_PICTURE_VEHICLE);*/
+
+
+        File fileimg = new File(Environment.getExternalStorageDirectory(),ROUTE_IMAGE);
+        Boolean iscreate=fileimg.exists();
+        String name_img_vehicle = "";
+
+        if(iscreate==false){
+            iscreate=fileimg.mkdirs();
+        }
+
+        if (iscreate==true){
+            name_img_vehicle = (System.currentTimeMillis() / 1000) + ".jpg";
+        }
+
+        path = Environment.getExternalStorageDirectory()+File.separator+ROUTE_IMAGE+File.separator+name_img_vehicle;
+
+        File img_vehicle = new File(path);
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(img_vehicle));
+        startActivityForResult(intent,CODE_SELECTED_CAMERA);
+
     }
 
         /*protected void onActivityResult(int requestCode, int resultCode,
@@ -216,8 +255,31 @@ public class EditorAddVehicle extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode==RESULT_OK){
-            Uri  path = data.getData();
-            img_vehicle.setImageURI(path);
+
+            switch (requestCode){
+
+                case CODE_SELECTED_GALLERY:
+                    Uri  mypath = data.getData();
+                    img_vehicle.setImageURI(mypath);
+                    break;
+
+                case CODE_SELECTED_CAMERA:
+                    MediaScannerConnection.scanFile(this, new String[]{path}, null,
+                            new MediaScannerConnection.OnScanCompletedListener() {
+                        @Override
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.i("storage path","Path: "+path);
+                        }
+                    });
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(path);
+                    img_vehicle.setImageBitmap(bitmap);
+
+
+                    break;
+
+            }
+
         }
     }
 }
